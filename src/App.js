c9e789e3433e284b142739e87bd3b9776f7db048
Hsmahbub <hsmahbub@gmail.com>
2023-05-09 06:37:07 +0600

    hit toast function and fetch url issue fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ComingSoon from './components/ComingSoon';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 function App() {
   return (
     <>
--- src/helpers/hitToast.js
+++ src/helpers/hitToast.js
@@ -1,14 +1,14 @@
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 const hitToast = (variant, message) => {
-    if (!['success', 'error'].includes(variant)) throw Error();
-    toast[`${variant}`](message, {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-    });
-}
+	if (!["success", "error"].includes(variant)) throw Error();
+	toast[`${variant}`](message, {
+		position: "top-right",
+		autoClose: 3000,
+		hideProgressBar: false,
+		closeOnClick: true,
+		pauseOnHover: true,
+		draggable: true,
+		progress: undefined,
+	});
+};
 export default hitToast;
